0e72d2b4ecea6896c6e91decf13c3910ec34742f
Jake Marsh <jakemmarsh@gmail.com>
2015-09-24 17:19:32 -0700

    update deploy task, fix to remove extensions from HTML files

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,18 +1,22 @@
 'use strict';
 
-var gulp       = require('gulp');
-var awspublish = require('gulp-awspublish');
-var dotenv     = require('dotenv');
+var gulp             = require('gulp');
+var rename           = require('gulp-rename');
+var awspublish       = require('gulp-awspublish');
+var awspublishRouter = require('gulp-awspublish-router');
+var dotenv           = require('dotenv');
 
 dotenv.load();
 
 gulp.task('deploy', ['prod'], function() {
 
   var publisher = awspublish.create({
-    key: process.env.AWS_KEY,
-    secret: process.env.AWS_SECRET,
-    bucket: process.env.S3_BUCKET,
-    region: process.env.S3_REGION
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+    params: {
+      Bucket: process.env.S3_BUCKET,
+      Region: process.env.S3_REGION
+    }
   });
   var oneWeekInSeconds = 60*60*24*7;
   var headers = {
@@ -21,6 +25,25 @@ gulp.task('deploy', ['prod'], function() {
 
   // Assets
   return gulp.src('./build/**/*')
+  .pipe(rename(function(path) {
+    // Remove extension from all HTML files for cleaner routes on S3
+    if ( path.basename !== 'index' && path.extname === '.html' ) {
+      path.extname = '';
+    }
+  }))
+  .pipe(awspublishRouter({
+    routes: {
+      // Assume all files without an extension are HTML files
+      '^([^.]+)$': {
+        headers: {
+          'Content-Type': 'text/html'
+        }
+      },
+      '^.+$': {
+        key: '$&'
+      }
+    }
+  }))
   .pipe(awspublish.gzip())
   .pipe(publisher.publish(headers))
   .pipe(awspublish.reporter());
