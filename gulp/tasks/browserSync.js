c0c65d472dfadc56895f8157e1cbe96e382c4bad
Jake Marsh <jakemmarsh@gmail.com>
2015-11-08 15:17:15 -0800

    user browser-sync for server, remove express dependency

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -1,17 +1,36 @@
 'use strict';
 
-var config      = require('../config');
+var url         = require('url');
 var browserSync = require('browser-sync');
 var gulp        = require('gulp');
+var config      = require('../config');
 
 gulp.task('browserSync', function() {
 
-  browserSync({
+  var DEFAULT_FILE = 'index.html';
+  var ASSET_EXTENSIONS = ['js', 'css', 'png', 'jpg', 'jpeg', 'gif'];
+
+  browserSync.init({
+    server: {
+      baseDir: config.dist.root,
+      middleware: function(req, res, next) {
+        var fileHrefArray = url.parse(req.url).href.split('.');
+        var fileExtension = fileHrefArray[fileHrefArray.length - 1];
+
+        if ( ASSET_EXTENSIONS.indexOf(fileExtension) === -1 ) {
+          req.url = '/' + DEFAULT_FILE;
+        }
+
+        return next();
+      }
+    },
     port: config.browserPort,
     ui: {
       port: config.UIPort
     },
-    proxy: 'localhost:' + config.serverPort
+    ghostMode: {
+      links: false
+    }
   });
 
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@
 var gulp   = require('gulp');
 var config = require('../config');
 
-gulp.task('watch', ['browserSync', 'server'], function() {
+gulp.task('watch', ['browserSync'], function() {
 
   gulp.watch(config.styles.src,  ['sass']);
   gulp.watch(config.fonts.src,   ['copyFonts']);
